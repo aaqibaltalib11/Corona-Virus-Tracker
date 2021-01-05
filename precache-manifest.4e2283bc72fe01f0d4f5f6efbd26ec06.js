afbffec097801abdf5fe5b006d8a230041570a72
Aaqib Altalib <75214899+aaqibaltalib11@users.noreply.github.com>
2021-01-05 15:55:22 -0800

    Add files via upload

---
+++ precache-manifest.4e2283bc72fe01f0d4f5f6efbd26ec06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8cc9e86665aec0ad88b71206928225",
+    "url": "/Covid-19/index.html"
+  },
+  {
+    "revision": "300024446c97c92e8be5",
+    "url": "/Covid-19/static/css/main.b4bc2bbb.chunk.css"
+  },
+  {
+    "revision": "ccf7630734ece9de6dc7",
+    "url": "/Covid-19/static/js/2.a500990f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19/static/js/2.a500990f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "300024446c97c92e8be5",
+    "url": "/Covid-19/static/js/main.ddb7b701.chunk.js"
+  },
+  {
+    "revision": "3d3d22c1dad91e5a0108",
+    "url": "/Covid-19/static/js/runtime-main.a9f07f34.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19/static/media/image.d7265326.png"
+  }
+]);
